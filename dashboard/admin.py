f8f9b9d62b5715798580977719a8f3ba328543b8
KhasanWEB <hasanchechen4622@gmail.com>
2024-03-02 19:06:43 +0300

    Check: update, delete, create And more

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from dashboard.models import Category, Payment
+from dashboard.models import Category, Payment,  Check
 
 admin.site.register(Category)
 admin.site.register(Payment)
+admin.site.register(Check)
+
--- dashboard/models.py
+++ dashboard/models.py
@@ -5,13 +5,25 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Check(models.Model):
+    name = models.CharField('Название', max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    money = models.IntegerField('Сумма')
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+    class Meta:
+        verbose_name = 'Счет'
+        verbose_name_plural = 'Счеты'
+
 
 class Category(models.Model):
     name = models.CharField('Название', max_length=100)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
-    def __str__(self) -> models.CharField:
-        return self.name
+    def __str__(self) -> str:
+        return str(self.name)
 
     class Meta:
         verbose_name = 'Категория'
@@ -22,10 +34,22 @@ class Payment(models.Model):
     category = models.ForeignKey(
         'Category',
         on_delete=models.CASCADE,
-        related_name='payments'
+        related_name='payments',
+        null=True
     )
+    payment_check = models.ForeignKey(
+        'Check',
+        on_delete=models.CASCADE,
+        related_name='payments',
+        null=True
+    )
+
     summa = models.PositiveIntegerField('Сумма')
+    is_payment = models.BooleanField(default=True)
     data = models.DateField('Дата', default=datetime.datetime.now)
+
+    def __str__(self) -> str:
+        return str(self.summa)
 
     class Meta:
         verbose_name = 'Платеж'
